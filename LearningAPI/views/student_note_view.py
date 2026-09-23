94b124e5139dea9652bf4966ac4595e96a8ab48d
James Freeman <jamesfreeman114@gmail.com>
2026-09-23 12:20:07 -0500

    add structlog tracing to student note view

--- LearningAPI/views/student_note_view.py
+++ LearningAPI/views/student_note_view.py
@@ -4,7 +4,9 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from LearningAPI.models.people import NssUser
 from LearningAPI.models.people import StudentNote, StudentNoteType
+import structlog
 
+logger = structlog.get_logger(__name__)
 
 class StudentNoteViewSet(ModelViewSet):
     """Student note viewset"""
@@ -12,6 +14,7 @@ class StudentNoteViewSet(ModelViewSet):
 
 
     def list(self, request):
+        logger.info("Student Notes List Method Initiated", data=request.data)
         studentId = request.query_params.get('studentId', None)
 
         if studentId is None:
@@ -23,6 +26,7 @@ class StudentNoteViewSet(ModelViewSet):
             return Response({'message': 'Invalid student id.'}, status=status.HTTP_404_NOT_FOUND)
 
         notes = StudentNote.objects.filter(student=student)
+        logger.info("Student Notes filtered by Student id.", student_notes=len(notes))
         data = StudentNoteSerializer(notes, many=True).data
         return Response(data, status=status.HTTP_200_OK)
 
@@ -32,7 +36,12 @@ class StudentNoteViewSet(ModelViewSet):
         Returns:
             Response -- JSON serialized instance
         """
+
+        logger.info("New Student Note Create Method Initiated", data=request.data)
+
         student = NssUser.objects.get(pk=request.data['studentId'])
+        logger.info("Student Id Found", student_id=student.id)
+
         coach = NssUser.objects.get(user=request.auth.user)
 
         note_text = request.data.get('note', None)
@@ -46,6 +55,8 @@ class StudentNoteViewSet(ModelViewSet):
 
         try:
             student_note_type = StudentNoteType.objects.get(pk=note_type)
+            
+           
         except StudentNoteType.DoesNotExist:
             return Response({"reason": 'Invalid note type.'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -55,7 +66,10 @@ class StudentNoteViewSet(ModelViewSet):
             note.coach = coach
             note.note = note_text
             note.note_type = student_note_type
+            logger.info("Student Note Foreign Key Resolved", student_id=note.student.id, note_type=note.note_type.label)
             note.save()
+
+            logger.info("Student Note Saved to Database", note_id = note.id)
 
             serializer = StudentNoteSerializer(note)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
